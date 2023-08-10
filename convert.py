803469016666e194817ab62496d84255e06b9329
Tiago Possato <dev.tiago.possato@gmail.com>
2023-08-10 13:29:41 -0300

    update nos arquivos acessórios

--- convert.py
+++ convert.py
@@ -1,39 +1,11 @@
 import subprocess
 import os
-import re
-import shutil
+from utils import copy_directory, remove_directory, copy_file
 
-input_file = 'descmaker\supremica_examples\example.wmod'
+input_file = 'pick_and_place_basic.wmod'
 
 output_dir = 'supervisor_tmp'
 
-def copy_directory(source, destination, exclude_files=None, exclude_dirs=None):
-    if os.path.exists(destination):
-        shutil.rmtree(destination)
-    os.makedirs(destination)
-
-    for item in os.listdir(source):
-        source_path = os.path.join(source, item)
-        destination_path = os.path.join(destination, item)
-
-        if os.path.isfile(source_path):
-            # Skip the excluded files based on regex pattern
-            # print(f"Copying file: {source_path}, item: {item}")
-            if exclude_files and any(re.match(pattern, item) for pattern in exclude_files):
-                # print(f"Skipping file: {source_path}")
-                continue
-
-            # Copy files
-            shutil.copy2(source_path, destination_path)
-        elif os.path.isdir(source_path):
-            # Skip the excluded directories based on regex pattern
-            # print(f"Copying dir: {source_path}")
-            if exclude_dirs and any(re.match(pattern, item) for pattern in exclude_dirs):
-                # print(f"Skipping dir: {source_path}")
-                continue
-
-            # Recursively copy directories
-            copy_directory(source_path, destination_path, exclude_files, exclude_dirs)
 
 # verify if '../descmaker/app/descmaker.py' exists
 if not os.path.exists('descmaker/app/descmaker.py'):
@@ -44,16 +16,15 @@ if not os.path.exists('descmaker/app/descmaker.py'):
     result.wait()
     if(result.returncode != 0):
         print("\n\t---> Error updating submodule!\n")
-        exit(0)
+        exit(-1)
     else:
         print("\n\t---> Submodule updated successfully!\n")
-
 
 result = subprocess.Popen(['python', 'descmaker/app/descmaker.py', '-i', input_file, '-o', output_dir])
 result.wait()
 if(result.returncode != 0):
     print("\n--->Error running descmaker!\n")
-    exit(0)
+    exit(-1)
 else:
     print("\n--->descmaker executed successfully!\n")
 
@@ -63,4 +34,6 @@ copy_directory(output_dir + '/lib/supervisor', 'application/lib/supervisor')
 # copy output_dir/src/supervisors to application/src/supervisors
 copy_directory(output_dir + '/src/supervisors', 'application/src/supervisors')
 
-shutil.rmtree(output_dir)
+copy_file(output_dir + '/src/event_handler/events.h', 'application/src/event_handler/events.h')
+
+remove_directory(output_dir)
--- setup.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-
-def setup():
-
-if __name__ == '__main__':
-    setup()
